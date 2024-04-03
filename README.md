084e4742ca111444fa32a77a6a9d95ca2b24bf6f
hjiang13 <hjiang13@kent.edu>
2024-04-03 14:21:10 -0400

    Initialized as CodeBERT-classification

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CODEBERT-REGRESSION
-To apply CodeBERT to regression task
+This project is to predict the resilience level of c/cpp programs by fin-tuning CodeBERT-cpp model (https://huggingface.co/neulab/codebert-cpp).
+
+We both do classification prediction and regression prediction
+
+The Data set is here: https://github.com/hjiang13/DARE
---
+++ code/train_resilience.sh
@@ -0,0 +1,16 @@
+python run.py \
+    --output_dir=./saved_models \
+    --tokenizer_name=neulab/codebert-cpp \
+    --model_name_or_path=neulab/codebert-cpp \
+    --do_train \
+    --do_test \
+    --train_data_file=../dataset/train_resilience.jsonl \
+    --eval_data_file=../dataset/valid_resilience.jsonl \
+    --test_data_file=../dataset/test_resilience.jsonl \
+    --num_train_epochs 5 \
+    --block_size 256 \
+    --train_batch_size 4 \
+    --eval_batch_size 4 \
+    --learning_rate 2e-5 \
+    --max_grad_norm 1.0 \
+    --seed 123456  2>&1 | tee train.log

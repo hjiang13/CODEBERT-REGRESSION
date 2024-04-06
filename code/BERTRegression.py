ead9b1cef0e5a4fe23a99e0c33ce07823cf763ee
hjiang13 <hjiang13@kent.edu>
2024-04-05 22:59:06 -0400

    test on tiny dataset

--- code/BERTRegression.py
+++ code/BERTRegression.py
@@ -22,7 +22,7 @@ train_data = pd.DataFrame( {"code": [], "label": []})
 val_data = pd.DataFrame( columns=['code', 'label'])
 
 with open(trainDataPath, "r") as data_file:
-
+    i = 0
     for line in data_file:
         line = json.loads(line)
         print(line["code"] + "\n" )
@@ -30,14 +30,20 @@ with open(trainDataPath, "r") as data_file:
         lineList= [[line["code"], line["label"]]]
         df_line = pd.DataFrame(lineList, columns=['code', 'label'])
         train_data = pd.concat([train_data, df_line])
+        i += 1
+        if i > 5:
+            break
 
 with open(evalDataPath, "r") as data_file:
-
+    i = 0
     for line in data_file:
         line = json.loads(line)
         lineList= [[line["code"], line["label"]]]
         df_line = pd.DataFrame(lineList, columns=['code', 'label'])
         val_data = pd.concat([val_data, df_line])
+        i += 1
+        if i > 5:
+            break
 
 
 #data = pd.read_json("../dataset/SDC_train_resilience_r.jsonl")

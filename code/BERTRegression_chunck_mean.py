28c01313bb9654ad3340f266f8f560bb2578bb55
hjiang13 <hjiang13@kent.edu>
2024-04-10 12:05:24 -0400

    gpu added

--- code/BERTRegression_chunck_mean.py
+++ code/BERTRegression_chunck_mean.py
@@ -128,9 +128,10 @@ loss_fn = nn.MSELoss()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 n_gpu = torch.cuda.device_count()
+model.to(device)
 if n_gpu > 1:
     model = torch.nn.DataParallel(model)
-    
+
 # Train the model
 model.train()
 best_acc = 0.0
